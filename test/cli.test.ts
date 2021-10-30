58d8932d7cd9c7793375d67542d06cd080ab9f2d
stephan <stephanruler@gmail.com>
2021-10-30 13:57:06 +0200

    fix: update test

--- test/cli.test.ts
+++ test/cli.test.ts
@@ -2,7 +2,7 @@ import execa from 'execa'
 
 test('main', async () => {
 	console.log(process.cwd())
-	const {stdout} = await execa('./dist/cli.js', ['--help'])
+	const {stdout} = await execa('./dist/Cli.js', ['--help'])
 	// const {stdout} = await execa('ls', ['./dist'])
 	console.log('stdout', stdout)
 	// expect(stdout).toEqual('ponies & rainbows')
